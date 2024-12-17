e592d91679ba854b13b2988f592eec34a606e0c2
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-17 19:29:13 +0530

    Added Ethereum Wallet

--- web-wallet/src/EthereumWallet.jsx
+++ web-wallet/src/EthereumWallet.jsx
@@ -1,3 +1,33 @@
 import { useState } from "react";
 import { mnemonicToSeed } from "bip39";
 import { Wallet , HDNodeWallet} from "ethers";
+
+export const EthereumWallet = ({mnemonic}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [addresses, setAddresses] = useState([]);
+
+   return (
+
+    <>
+<button onClick = {async function(){
+        const seed = await mnemonicToSeed(mnemonic);
+        const derivationPath = `m/44'/60'/${currentIndex}'/0'`;
+        const hdNode = HDNodeWallet.fromSeed(seed);
+        const child = hdNode.derivePath(derivationPath);
+        const privateKey = child.privateKey;
+        const wallet = new Wallet(privateKey);  
+        setCurrentIndex(currentIndex + 1);
+        setAddresses([...addresses, wallet.address]);
+
+    }}> Generate Ethereum Wallet</button>
+    
+
+    {addresses.map(p => <div>
+        Eth - {p}
+    </div>)}
+
+    </>
+    
+
+   )
+}
