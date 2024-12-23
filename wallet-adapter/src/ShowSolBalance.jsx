0877b5cf17937ed12237bc8df37bd13c925ab075
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-23 11:33:35 +0530

    Show Solana Balance

--- wallet-adapter/src/ShowSolBalance.jsx
+++ wallet-adapter/src/ShowSolBalance.jsx
@@ -1,1 +1,21 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+export function ShowSolBalance() {
+    const {connection} = useConnection();
+    const wallet = useWallet();
+
+    async function getBalance(){
+        let balance = await connection.getBalance(wallet.publicKey);
+        document.getElementById("balance").innerHTML = balance / LAMPORTS_PER_SOL;  
+        alert("Balance: " + balance / LAMPORTS_PER_SOL + " SOL");
+    }
+
+    return (
+        <div>
+            <h1>Check Balance</h1>
+            <button onClick={getBalance}>Check Balance</button>
+            <p id="balance"></p>
+        </div>
+    )
+}
