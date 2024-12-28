f3fa1e183cd82807d60e01ec54d757bb791c61bc
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-29 03:41:02 +0530

    Project setup

---
+++ token-launchpad/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchPad'
+
+function App() {
+  return (
+    <TokenLaunchpad></TokenLaunchpad>
+  )
+}
+
+export default App
