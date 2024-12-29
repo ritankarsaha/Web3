e7a3f58b27482414ef83a896f8d92416d4f0f21d
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-30 00:05:16 +0530

    Making the Token LaunchPad

--- token-launchpad/src/components/TokenLaunchPad.jsx
+++ token-launchpad/src/components/TokenLaunchPad.jsx
@@ -1,5 +1,29 @@
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { MINT_SIZE, TOKEN_2022_PROGRAM_ID, createMintToInstruction, createAssociatedTokenAccountInstruction, getMintLen, createInitializeMetadataPointerInstruction, createInitializeMintInstruction, TYPE_SIZE, LENGTH_SIZE, ExtensionType, mintTo, getOrCreateAssociatedTokenAccount, getAssociatedTokenAddressSync } from "@solana/spl-token"
+import { createInitializeInstruction, pack } from '@solana/spl-token-metadata';
+
+
 
 export function TokenLaunchpad() {
+
+    const connection = useConnection();
+    const wallet = useWallet();
+
+    async function createToken() {
+        const mintKeypair = Keypair.generate();
+        const metadata = {
+            mint: mintKeypair.publicKey,
+            name: 'RITA',
+            symbol: 'RIT',
+            uri: 'https://cdn.100xdevs.com/metadata.json',
+            additionalMetadata: [],
+        };
+
+    
+    }
+
+    
     return  <div style={{
         height: '100vh',
         display: 'flex',
