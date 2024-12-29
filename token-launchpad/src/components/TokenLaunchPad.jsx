95089f369a0dc8cd401a3140dc0950591f8b16e4
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-30 00:25:17 +0530

    Added transactions

--- token-launchpad/src/components/TokenLaunchPad.jsx
+++ token-launchpad/src/components/TokenLaunchPad.jsx
@@ -3,8 +3,6 @@ import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { MINT_SIZE, TOKEN_2022_PROGRAM_ID, createMintToInstruction, createAssociatedTokenAccountInstruction, getMintLen, createInitializeMetadataPointerInstruction, createInitializeMintInstruction, TYPE_SIZE, LENGTH_SIZE, ExtensionType, mintTo, getOrCreateAssociatedTokenAccount, getAssociatedTokenAddressSync } from "@solana/spl-token"
 import { createInitializeInstruction, pack } from '@solana/spl-token-metadata';
 
-
-
 export function TokenLaunchpad() {
 
     const connection = useConnection();
@@ -20,8 +18,52 @@ export function TokenLaunchpad() {
             additionalMetadata: [],
         };
 
-    
+
+        const mintLen = getMintLen(ExtensionType.MetadataPointer);
+        const metadataLen = pack(metadata).length + TYPE_SIZE + LENGTH_SIZE;
+        const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
+        const transaction = new Transaction().add(
+        SystemProgram.createAccount({
+            fromPubkey: wallet.publicKey,
+            newAccountPubkey: mintKeypair.publicKey,
+            space: mintLen,
+            lamports,
+            programId: TOKEN_2022_PROGRAM_ID,
+        }),
+
+        
+        createInitializeMetadataPointerInstruction(mintKeypair.publicKey,wallet.publicKey,mintKeypair.publicKey,TOKEN_2022_PROGRAM_ID),
+        createInitializeMintInstruction(mintKeypair.publicKey,9,wallet.publicKey,null,TOKEN_2022_PROGRAM_ID),
+        createInitializeInstruction({
+            programId: TOKEN_2022_PROGRAM_ID,
+            mint: mintKeypair.publicKey,
+            metadata: mintKeypair.publicKey,
+            name: metadata.name,
+            symbol: metadata.symbol,
+            uri: metadata.uri,
+            mintAuthority: wallet.publicKey,
+            updateAuthority: wallet.publicKey,
+            creators: ['Ritankar Saha'],
+
+        }),
+
+
+
+    );
+
+    transaction.feePayer = wallet.publicKey;
+    transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+    transaction.partialSign(mintKeypair);
+
+
+    await wallet.sendTransaction(transaction, connection);
+
+
+
     }
+    
+    
+
 
     
     return  <div style={{
@@ -36,6 +78,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
